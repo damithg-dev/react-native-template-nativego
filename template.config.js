ac439d87e018775421d56164930517dff017d4f8
Damith <gdan994@gmail.com>
2021-01-23 11:23:04 +0530

    updated template config

---
+++ template.config.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+    // Placeholder name that will be replaced in package.json, index.json, android/, ios/ for a project name.
+    placeholderName: 'HelloWorld',
+  
+    // Placeholder title that will be replaced in values.xml and Info.plist with title provided by the user.
+    // We default this value to 'Hello App Display Name', which is default placeholder in react-native template.
+    titlePlaceholder: 'HelloWorld',
+  
+    // Directory with the template which will be copied and processed by React Native CLI. Template directory should have package.json with all dependencies specified, including `react-native`.
+    templateDir: './',
+  
+    // Path to script, which will be executed after initialization process, but before installing all the dependencies specified in the template. This script runs as a shell script but you can change that (e.g. to Node) by using a shebang (see example custom template).
+    // postInitScript: './script.js',
+  };
+  
